f40ae68dbea307d6712b41a82857b4539754130b
650volvo <volvo650@yahoo.com>
2017-11-27 15:26:43 +0800

    Create app.blade.php

---
+++ app.blade.php
@@ -0,0 +1,16 @@
+<?php
+?>
+<html>
+<head>
+<style>
+h1 {color:red;}
+p {color:blue;}
+</style>
+</head>
+<body>
+<title>App Name - @yield('title')</title>
+<h1>A heading</h1>
+<p>A paragraph.</p>
+@yield('content')
+</body>
+</html>
